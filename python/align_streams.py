241fd7af378d00ea4dfe97646babeb7ffa524d1c
cmayer <Christoph.Mayer@cern.ch>
2019-01-16 10:36:05 +0100

    documentation added

--- python/align_streams.py
+++ python/align_streams.py
@@ -26,7 +26,8 @@ import pmt
 
 class find_offsets(gr.sync_block):
     """
-    docstring for block find_offsets
+    This block determines offsets (number of samples) between a number of streams using rx_time tags
+    Note that it assumes that the offsets are close to integer multiples of samples
     """
     def __init__(self, num_streams):
         gr.sync_block.__init__(self,
@@ -35,7 +36,7 @@ class find_offsets(gr.sync_block):
                                out_sig = gr.py_io_signature(1, -1, num_streams*(np.complex64,)))
         self._num_streams = num_streams
         self._tags = [[] for _ in range(num_streams)]
-        self._fs   = 12001*np.ones(num_streams, dtype=np.double)
+        self._fs   = 12001*np.ones(num_streams, dtype=np.double) ## default sample rate
         self._offsets = np.zeros(num_streams, dtype=np.int)
         self._tags_new = [False for _ in range(num_streams)]
         self._port_delay = pmt.intern('delay')
@@ -44,13 +45,10 @@ class find_offsets(gr.sync_block):
 
     def work(self, input_items, output_items):
         n = len(output_items[0])
-        for i in range(self._num_streams):
-            output_items[i][:] = input_items[i]
-
         tags=[[] for _ in range(self._num_streams)]
         f = lambda x : x[0]+x[1]
         for i in range(self._num_streams):
-            ## tags[i][0] => abs sample#
+            ## tags[i][0] => absolute sample#
             ## tags[i[[1] => GNSS timestamp
             tags[i] = [(x.offset, f(pmt.to_python(x.value)))
                        for x in self.get_tags_in_window(i, 0, n)
@@ -61,30 +59,35 @@ class find_offsets(gr.sync_block):
                 self._tags[i] = tags[i][0]
                 self._tags_new[i] = True
         if all(self._tags_new):
-            #print('tags_new:', self._tags)
-            ## compute differences w.r.t 1st
+            ## compute differences w.r.t 1st in seconds
             fd = lambda x,y,fsx,fsy:(x[0]/fsx-y[0]/fsy) - (x[1]-y[1])
             dd = np.array([fd(self._tags[i], self._tags[0], self._fs[i], self._fs[0])
                            for i in range(1,self._num_streams)])
+
+            ## differences in samples
             dds = dd * self._fs[1:]
+
+            ## compute the offsets avoiding negative delays
             self._offsets[0]  = 0
             self._offsets[1:] = np.round(dds)
             self._offsets    -= np.min(self._offsets)
-            print('dd= ', dd, dds, np.round(dds), (dds-np.round(dds))/self._fs[1:]*1e9, self._offsets)
             self._tags_new = [False for _ in range(self._num_streams)]
 
-            ## set offsets to delay taps
+            ## publish the offsets to the message port
             msg_out = pmt.make_dict()
             msg_out = pmt.dict_add(msg_out, pmt.intern('delays'), pmt.to_pmt([x for x in self._offsets]))
-            #print(msg_out)
             self.message_port_pub(self._port_delay, msg_out)
 
-        #print(tags)
+        ## pass through all data streams
+        for i in range(self._num_streams):
+            output_items[i][:] = input_items[i]
+
         return n
 
 class delay_array(gr.basic_block):
     """
-    docstring for block find_offsets
+    This is a pure message handling gr.basic_block containing an array of blocks.delay
+    The delay of these blocks is set in the message handler
     """
     def __init__(self, num_streams):
         gr.basic_block.__init__(
@@ -104,20 +107,16 @@ class delay_array(gr.basic_block):
         return self._delays[i]
 
     def msg_handler_delay(self, msg_in):
-        #print('-------------------- msg_handler_delay --------------------')
-        #print('msg_in= ', msg_in)
         delays = pmt.to_python(pmt.cdar(msg_in))
-        print('delays=', delays)
         for (i,d) in enumerate(delays):
-            print('delay',i,self._delays[i].dly(), d)
             self._delays[i].set_dly(d)
 
 class align_streams(gr.hier_block2):
     """
-    docstring for block physical_layer_driver
+    Block for aligning KiwiSDR IQ streams with GNSS timestamps
+    Note that the IQ streams have to be from the same KiwiSDR
     """
     def __init__(self, num_streams):
-        print '__init__', num_streams
         gr.hier_block2.__init__(self,
                                 "align_streams",
                                 gr.io_signature(num_streams, num_streams, gr.sizeof_gr_complex),
